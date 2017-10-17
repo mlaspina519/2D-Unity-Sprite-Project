ad2985f6bf16d3f912d11fbd0b650989caf30bf2
Matthew LaSpina <mlaspina519@gmail.com>
2017-10-16 20:13:27 -0400

    First Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private float horizontalMovement;
+	private float verticalMovement;
+	private float speed = 7.0f;
+    private bool facingRight = false;
+    private  Rigidbody2D rigid;
+    public GameObject bullet;
+
+	// Initialization
+	void Start() {
+            rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// User input - Once per frame
+	void Update () {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+
+        // Fires a bullet
+        if(Input.GetButtonDown("Fire1")) {
+            OnFireInput();
+        }
+	}
+
+	// Physics/Movement - fixed rate
+    void FixedUpdate() {
+        rigid.velocity = new Vector2(verticalMovement * speed, rigid.velocity.y);
+        rigid.velocity = new Vector2(horizontalMovement * speed, rigid.velocity.x);
+
+		// Flips plane when changing directions
+		if (horizontalMovement < 0 && facingRight || horizontalMovement > 0 && !facingRight) {
+			transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            facingRight = !facingRight;
+		}
+    }
+
+    // Shoots bullet 
+    void OnFireInput() {
+        Vector2 position = new Vector2(transform.localScale.x, transform.localScale.y);
+
+        // Shoots in correct direction
+        if(facingRight) {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
